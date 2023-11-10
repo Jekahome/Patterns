dee664ae59bee78caca88a16fb80e5d61cabdd95
Jeka <yaroshjeka@gmail.com>
2023-11-10 19:21:15 +0100

    add fold

--- src/patterns/behavior/visitor/examples/main.rs
+++ src/patterns/behavior/visitor/examples/main.rs
@@ -36,11 +36,11 @@ use ast::*;
 // Пример конкретной реализации — обход AST, интерпретирующий его как код.
 pub struct Interpreter;
 impl Visitor<i64> for Interpreter {
-    fn visit_name(&mut self, n: &Name) -> i64 { panic!() }
+    fn visit_name(&mut self, n: &Name) -> i64 { panic!() }// нет смысла String приводить к i64
     fn visit_stmt(&mut self, s: &Stmt) -> i64 {
         match *s {
             Stmt::Expr(ref e) => self.visit_expr(e),
-            Stmt::Let(..) => unimplemented!(),
+            Stmt::Let(..) => unimplemented!(),// нет смысла String приводить к i64
         }
     }
 

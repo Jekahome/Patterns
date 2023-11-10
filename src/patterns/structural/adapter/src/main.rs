ca3792b0c1985203659ae01026147e0f2ae17f77
Jeka <yaroshjeka@gmail.com>
2023-11-10 21:50:41 +0100

    add adapter

--- src/patterns/structural/adapter/src/main.rs
+++ src/patterns/structural/adapter/src/main.rs
@@ -1,49 +1,90 @@
+/*
 
-// Адаптер  позволяет объектам с несовместимыми интерфейсами работать вместе.
-//Это объект-переводчик, который трансформирует интерфейс или данные одного объекта в такой вид, чтобы он стал понятен другому объекту.
+`Template Adapter` работает как мост между двумя несовместимыми интерфейсами.
+Эту стратегию можно реализовать с помощью наследования, с помощью агрегации, или композиции, то есть делегирования работы другим объектам.
+`Template Adapter` делает два несовместимых интерфейса совместимыми без изменения их существующего кода позволяя им работать вместе.
+Adapter это объект-переводчик, который трансформирует интерфейс или данные одного объекта в такой вид, чтобы он стал понятен другому объекту.
+При этом адаптер оборачивает один из объектов, так что другой объект даже не знает о наличии первого.
+Например, вы можете обернуть объект, работающий в метрах, адаптером, который бы конвертировал данные в футы.
 
-struct Banner {
-    string: String,
+Адаптеры могут не только переводить данные из одного формата в другой, но и помогать объектам с разными интерфейсами работать сообща.
+
+Это работает так:
+1. `Template Adapter` имеет интерфейс, который совместим с одним из объектов.
+2. Поэтому этот объект может свободно вызывать методы адаптера.
+3. `Template Adapter` получает эти вызовы и перенаправляет их второму объекту, но уже в том формате и последовательности, которые понятны второму объекту.
+
+
+Применимость
+- Когда вы хотите использовать сторонний класс, но его интерфейс не соответствует остальному коду приложения.
+- `Template Adapter` позволяет создать объект-прокладку, который будет превращать вызовы приложения в формат, понятный стороннему классу.
+
+**Отношения с другими паттернами**
+
+`Template Brige` проектируют загодя, чтобы развивать большие части приложения отдельно друг от друга.
+`Template Adapter` применяется постфактум, чтобы заставить несовместимые классы работать вместе.
+`Template Adapter` заставляет вещи работать после того, как они были спроектированы; `Template Brige` заставляет их работать изначально.
+
+`Template Facade` задаёт новый интерфейс, тогда как `Template Adapter` повторно использует старый.
+`Template Adapter` оборачивает только один класс, а `Template Facade` оборачивает целую подсистему.
+Кроме того, `Template Adapter` позволяет двум существующим интерфейсам работать сообща, вместо того, чтобы задать полностью новый.
+
+`Template Adapter` предоставляет другой интерфейс для своего объекта.
+`Template Proxy` предоставляет тот же интерфейс.
+`Template Decorator` предоставляет улучшенный интерфейс.
+
+`Template Adapter` предназначен для изменения интерфейса существующего объекта.
+`Template Decorator` улучшает другой объект, не меняя его интерфейс.
+Таким образом, декоратор более прозрачен для приложения, чем адаптер.
+Как следствие, `Template Decorator` поддерживает рекурсивную композицию, что невозможно при использовании чистых адаптеров.
+*/
+
+/*
+Интрументы работы со строками не совместимы, tools_a работает в асинхронной среде, а tools_b в синхронной
+Необходима возможность их соединить для совместной работы
+*/
+pub mod tools_a {
+    pub async fn add_wrap_start(arg: String) -> String {
+        std::future::ready(format!("~~~{}", arg)).await
+    }
 }
 
-impl Banner {
-    fn new(string: String) -> Banner {
-        Banner {
-            string: string,
+pub mod tools_b {
+    use std::string::FromUtf8Error;
+    pub fn add_wrap_end(arg: &[u8]) -> Result<String, FromUtf8Error> {
+        let s = String::from_utf8(arg.to_vec())?;
+        let res = format!("{}~~~", s);
+        Ok(res)
+    }
+}
+
+use adapter::*;
+pub mod adapter {
+    use crate::tools_b;
+
+    use futures::Future;
+    pub trait AdapterAsyncToSync {
+        fn add_wrap_end(fut: impl Future<Output = String>) -> String;
+    }
+    pub struct ImplAdapter;
+    impl AdapterAsyncToSync for ImplAdapter {
+        fn add_wrap_end(fut: impl Future<Output = String>) -> String {
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            let result = rt.block_on(fut);
+            if let Ok(res) = tools_b::add_wrap_end(result.as_bytes()) {
+                return res;
+            }
+            result
         }
-    }
-    fn show_with_paren(&self) {
-        println!("({})", self.string);
-    }
-    fn show_with_aster(&self) {
-        println!("*{}*", self.string);
-    }
-}
-
-struct PrintBanner {
-    banner: Banner,
-}
-impl PrintBanner {
-    fn new(string: String) -> PrintBanner {
-        PrintBanner {
-            banner: Banner::new(string),
-        }
-    }
-    fn print_weak(&self) {
-        self.banner.show_with_paren();
-    }
-    fn print_strong(&self) {
-        self.banner.show_with_aster();
     }
 }
 
 fn main() {
-    let p = PrintBanner::new("Hello".to_string());
-    p.print_weak();
-    p.print_strong();
+    let input = "hello";
+    let input_with_wrap = {
+        let input_a = tools_a::add_wrap_start(input.into());
+        let input_b: String = ImplAdapter::add_wrap_end(input_a);
+        input_b
+    };
+    assert_eq!("~~~hello~~~", input_with_wrap)
 }
-/*
-(Hello)
-*Hello*
-
-*/

68f3229d8b3186a507684ff35b5d3199949c5679
Jeka <yaroshjeka@gmail.com>
2023-11-10 11:50:34 +0100

    add visitor

--- src/patterns/behavior/visitor/src/main.rs
+++ src/patterns/behavior/visitor/src/main.rs
@@ -1,98 +1,159 @@
+#![allow(dead_code)]
+
 /*
-
-Пациент зовет врачей посетить его.
-
-`Template Visitor` позволяет добавлять в программу новые операции, не изменяя классы объектов, 
+`Template Visitor` позволяет добавлять в программу новые операции, не изменяя разнородные классы объектов,
 над которыми эти операции могут выполняться.
+(т.е. все струтуры или перечисления остаюся нетронутыми `Industrial,Residential,Commercial,Build,LevelBuild`)
 
-Над каждым объектом некоторой структуры выполняется одна или более операций. 
-Нужно определить новую операцию, не изменяя классы объектов.
-
-`Template Visitor` полезен везде, где вы хотите применить алгоритм к разнородным данным. 
-Если данные однородны, вы можете использовать шаблон, подобный Iterator. 
-Использование объекта посетителя (а не функционального подхода) позволяет посетителю сохранять состояние и, 
+`Template Visitor` полезен везде, где вы хотите применить алгоритм к разнородным данным.
+Если данные однородны, просто применяем один метод.
+Использование объекта посетителя (а не функционального подхода) позволяет посетителю сохранять состояние и,
 таким образом, передавать информацию между узлами.
+
+Т.е. в идеале мы бы могли имплементироваться от трейта и просто вызывая обший для всех метод решить свою задачу, но мы в силу каких-то
+причин так не можем делать.
+Так-же, возможная причина, это неуместная логика в рамках этих структур или потенциальные изменения в новой требуемой логике.
 
 `Template Fold` аналогичен `Template Visitor`, но создает новую версию посещенной структуры данных.
 */
 
+/*
+Проблема
+Ваша команда разрабатывает приложение, работающее с геоданными в виде графа.
+Узлами графа являются городские локации: памятники, театры, рестораны, важные предприятия и прочее.
+Каждый узел имеет ссылки на другие, ближайшие к нему узлы.
+Каждому типу узлов соответствует свой класс, а каждый узел представлен отдельным объектом.
 
-// Данные, которые мы не хотим изменять и поэтому будем их посещать
-mod ast {
-    pub enum Stmt {
-        Expr(Expr),
-        Let(Name, Expr),
+Ваша задача — сделать экспорт этого графа в XML. Дело было бы плёвым, если бы вы могли редактировать классы узлов.
+Достаточно было бы добавить метод экспорта в каждый тип узла, а затем, перебирая узлы графа, вызывать этот метод для каждого узла.
+Благодаря полиморфизму, решение получилось бы изящным, так как вам не пришлось бы привязываться к конкретным классам узлов.
+Но, к сожалению, классы узлов менять нельзя!
+*/
+use std::any::Any;
+
+// Узлы
+// for trait-object variants
+struct Industrial(String);
+struct Residential(Vec<u8>);
+struct Commercial([i32; 5]);
+
+// Узлы
+// for enum variants
+use build::*;
+mod build {
+    pub enum Build {
+        EIndustrial(Box<LevelBuild>),
+        EResidential(Box<LevelBuild>),
+        ECommercial(Box<LevelBuild>),
     }
-
-    pub struct Name {
-        value: String,
-    }
-
-    pub enum Expr {
-        IntLit(i64),
-        Add(Box<Expr>, Box<Expr>),
-        Sub(Box<Expr>, Box<Expr>),
-    }
-}
-
-// The abstract visitor
-// Интерфейс для всех типов используемых данных
-mod visit {
-    use super::*;
-    use ast::*;
-
-    pub trait Visitor<T> {
-        fn visit_name(&mut self, n: &Name) -> T;
-        fn visit_stmt(&mut self, s: &Stmt) -> T;
-        fn visit_expr(&mut self, e: &Expr) -> T;
-    }
-}
-
-use visit::*;
-use ast::*;
-
-// Пример конкретной реализации — обход AST, интерпретирующий его как код.
-pub struct Interpreter;
-impl Visitor<i64> for Interpreter {
-    fn visit_name(&mut self, n: &Name) -> i64 { panic!() }
-    fn visit_stmt(&mut self, s: &Stmt) -> i64 {
-        match *s {
-            Stmt::Expr(ref e) => self.visit_expr(e),
-            Stmt::Let(..) => unimplemented!(),
-        }
-    }
-
-    fn visit_expr(&mut self, e: &Expr) -> i64 {
-        match *e {
-            Expr::IntLit(n) => n,
-            Expr::Add(ref lhs, ref rhs) => self.visit_expr(lhs) + self.visit_expr(rhs),
-            Expr::Sub(ref lhs, ref rhs) => self.visit_expr(lhs) - self.visit_expr(rhs),
-        }
+    pub enum LevelBuild {
+        EFrameLevel(u8),
+        ENone,
     }
 }
 
-pub fn walk_expr(visitor: &mut Interpreter, e: &Expr) {
-    match *e {
-        Expr::IntLit(_) => {},
-        Expr::Add(ref lhs, ref rhs) => {
-            visitor.visit_expr(lhs);
-            visitor.visit_expr(rhs);
+// Aggregator
+struct CLientCity {
+    fields_1: Vec<Box<dyn Any>>,
+    fields_2: Vec<Build>,
+}
+
+// Необходимо имея неоднородную структуру типов, использовать конкретный метод для этого типа
+// В случае с коллекцией можем использовать только trait objects и как следствие downcast
+// Но в случае с enum просто matching
+impl CLientCity {
+    fn export_trait_objects(&self, export_visitor: impl VisitorForTraitObject) {
+        for item in self.fields_1.iter() {
+            if let Some(obj) = item.downcast_ref::<Industrial>() {
+                export_visitor.for_industrial(obj);
+            } else if let Some(obj) = item.downcast_ref::<Residential>() {
+                export_visitor.for_residential(obj);
+            } else if let Some(obj) = item.downcast_ref::<Commercial>() {
+                export_visitor.for_commercial(obj);
+            }
         }
-        Expr::Sub(ref lhs, ref rhs) => {
-            visitor.visit_expr(lhs);
-            visitor.visit_expr(rhs);
+    }
+    fn export_enum(&self, export_visitor: impl VisitorForEnum) {
+        for item in self.fields_2.iter() {
+            match item {
+                Build::EIndustrial(level) => {
+                    export_visitor.for_industrial(item);
+                    export_visitor.for_level(level);
+                }
+                Build::EResidential(level) => {
+                    export_visitor.for_residential(item);
+                    export_visitor.for_level(level);
+                }
+                Build::ECommercial(level) => {
+                    export_visitor.for_commercial(item);
+                    export_visitor.for_level(level);
+                }
+            }
         }
+    }
+}
+
+//------------------------------------------------------------------------------------------------------
+trait VisitorForTraitObject {
+    fn for_industrial(&self, obj: &Industrial);
+    fn for_residential(&self, obj: &Residential);
+    fn for_commercial(&self, obj: &Commercial);
+}
+struct ExportConcreteVisitor;
+impl VisitorForTraitObject for ExportConcreteVisitor {
+    fn for_industrial(&self, obj: &Industrial) {
+        println!("trait-obj export for_industrial {}", obj.0);
+    }
+    fn for_residential(&self, obj: &Residential) {
+        println!("trait-obj export for_residential {}", obj.0.len());
+    }
+    fn for_commercial(&self, obj: &Commercial) {
+        println!("trait-obj export for_commercial {:?}", obj.0);
+    }
+}
+
+//------------------------------------------------------------------------------------------------------
+trait VisitorForEnum {
+    fn for_industrial(&self, _e: &Build);
+    fn for_residential(&self, _e: &Build);
+    fn for_commercial(&self, _e: &Build);
+    fn for_level(&self, _e: &LevelBuild);
+}
+struct ExportConcreteVisitor2;
+impl VisitorForEnum for ExportConcreteVisitor2 {
+    fn for_industrial(&self, _e: &Build) {
+        println!("enum export for_industrial");
+    }
+    fn for_residential(&self, _e: &Build) {
+        println!("enum export for_residential");
+    }
+    fn for_commercial(&self, _e: &Build) {
+        println!("enum export for_commercial");
+    }
+    fn for_level(&self, _e: &LevelBuild) {
+        println!("enum export for_level");
     }
 }
 
 // cargo run --example p_behavior_visitor
-fn main(){
-    let exp = Expr::Add(Box::new(Expr::IntLit(8)),Box::new(Expr::IntLit(3)));
-    let mut interpreter = Interpreter;
-    let res = interpreter.visit_expr(&exp);
-    println!("{}",res);
+fn main() {
+    let export_visitor = ExportConcreteVisitor;
+    let export_visitor2 = ExportConcreteVisitor2;
 
-    let mut interpreter = Interpreter;
-    let exp = Expr::Add(Box::new(Expr::IntLit(8)),Box::new(Expr::IntLit(3)));
-    walk_expr(&mut interpreter,&exp);
+    let fields_1: Vec<Box<dyn Any>> = {
+        let some_city_object1 = Box::new(Industrial("Industrial".into()));
+        let some_city_object2 = Box::new(Residential(vec![1]));
+        let some_city_object3 = Box::new(Commercial([1, 2, 3, 4, 5]));
+        vec![some_city_object1, some_city_object2, some_city_object3]
+    };
+    let fields_2: Vec<Build> = {
+        vec![
+            Build::EIndustrial(Box::new(LevelBuild::EFrameLevel(1))),
+            Build::ECommercial(Box::new(LevelBuild::EFrameLevel(3))),
+        ]
+    };
+
+    let nodes_city_object: CLientCity = CLientCity { fields_1, fields_2 };
+    nodes_city_object.export_trait_objects(export_visitor);
+    nodes_city_object.export_enum(export_visitor2);
 }
